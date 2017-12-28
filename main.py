7890555495014543b675f78bfede0f011af64dbb
keyboom <pablohildo@gmail.com>
2017-12-28 20:00:24 -0300

    Typ

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ pidPs = psutil.Process(pid)
 usoMemoria = pidPs.memory_info()[0]
 prefixo = '!'
 bot = commands.Bot(command_prefix=prefixo, description='Teste')
+servidor = None
+natan = None
+
 @bot.event
 async def on_ready():
     print('Bot logado como')
     print(bot.user.name)
     print(bot.user.id)
     print(''.join('Utilizando '+str(usoMemoria)+' de RAM'))
-    
+    servidor = bot.get_server(str(258014545715396608))
+    natan = servidor.get_member_named('GoodMan215#5854')
+
+@bot.event
+async def on_member_join(natan):
+        await natan = servidor.get_member_named('GoodMan215#5854')
+        await bot.add_roles(natan,discord.utils.get(server.roles, name='Deveria Estar Banido'))
 
 @bot.command()
 async def kicknatan():
     servidor = bot.get_server(str(258014545715396608))
-    await bot.kick(servidor.get_member_named('GoodMan215#5854'))
+    await bot.kick(natan)
     await print('Natan foi kickado às '+datetime.datetime.now())
 
 bot.run('MjczNDU4NTg1OTA2NzA4NDgw.DRcmRw.rmATh3kOl6V_k_rhtKHywWzrC8E')
